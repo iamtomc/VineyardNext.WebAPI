c5287a889d0ab445ebbce1d88d3d8ba32dbabf45
Chad Smith <chad.e.smith@outlook.com>
2019-09-08 22:47:17 -0500

    updated EF

--- Models/VineyardNextContext.cs
+++ Models/VineyardNextContext.cs
@@ -31,7 +31,6 @@ namespace WebAPIApplication.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-
                 optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["VineyardNext"].ConnectionString);
             }
         }
